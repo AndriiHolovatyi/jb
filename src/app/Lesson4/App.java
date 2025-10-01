84fd3389094b3d941702bf711024cb0f594f3211
AndriiHolovatyi <kellercotler@gmail.com>
2025-10-01 21:45:27 +0300

    homework 4

---
+++ src/app/Lesson4/App.java
@@ -0,0 +1,31 @@
+package app.Lesson4;
+
+public class App {
+
+
+    public static void main() {
+
+        int salesDaysSmarphone = 5;
+        float totalSalesSmartphone = 12153.41f;
+        double dailySalesSmartphone = totalSalesSmartphone / salesDaysSmarphone;
+        int salesDaysLaptop = 7;
+        float dailySalesLaptop = 1498.12f;
+        double totalSalesLaptop = salesDaysLaptop * dailySalesLaptop;
+
+        System.out.printf("""
+               Product No %d: %s,
+               total sales for %d days is EUR %.2f,
+               sales by day is EUR %.2f.
+               """, 1, "smartphone", salesDaysSmarphone, totalSalesSmartphone, dailySalesSmartphone);
+
+        System.out.printf("""
+               Product No %d: %s,
+               total sales for %d days is EUR %.2f,
+               sales by day is EUR %.2f.
+               """, 2, "laptop", salesDaysLaptop, totalSalesLaptop, dailySalesLaptop);
+
+
+    }
+
+
+}
