9c85851d6dd2c46dbdd1a6ba8ea7e9534aa48e32
AndriiHolovatyi <kellercotler@gmail.com>
2025-10-09 22:55:05 +0300

    hw6

---
+++ src/Lesson6/App.java
@@ -0,0 +1,18 @@
+package Lesson6;
+
+public class App {
+
+    static void main(String[] args) {
+
+        var a = 0;
+        var sumOfNum = 0;
+        for (int i = 1; i <= 6; i++) {
+            sumOfNum += i;
+            System.out.println(i + ") Num is " + i + ", sum is " + sumOfNum);
+        }
+
+        System.out.println("-----------------------");
+        System.out.println("Sum of numbers is " + sumOfNum);
+
+    }
+}
